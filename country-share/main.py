e0284af3ddbf72f121f4e542d9663d37bd45d2f6
ErnestaP <ernesta.petraityte@yahoo.com>
2022-07-22 16:03:21 +0200

    ‘Country share script * Added tests * Fix ES config * Output of script is written in the csv file, current working dir. * Every time the gap file will be changed, don’t forget to put line on the end:UNKNOWN,UKNWN,"GDP per capita, PPP (current international $)",0 * ref: https://github.com/cern-sis/issues-scoap3/issues/72

--- country-share/main.py
+++ country-share/main.py
@@ -1,65 +1,30 @@
 import argparse
 import csv
 
-from opensearch_dsl import Q, Search
-from opensearchpy import OpenSearch, RequestsHttpConnection
+import urllib3
+from countries import full_country_list
+from elasticsearch import Elasticsearch
+from utils import (
+    build_countries_list_values,
+    get_affiliations,
+    get_authors,
+    get_gdp_from_file,
+    get_most_important_country,
+    get_primary_category,
+)
+
+urllib3.disable_warnings()
 
 
 def map_hit(hit):
-    # TODO: recid is currently wrong, it should be changed.
     return {
-        "doi": hit.dois,
-        "recid": hit.meta.id,
-        "journal": hit.publication_info[0].journal_title,
-        "creation_date": hit.record_creation_date,
-        # TODO: Add primary category
-        # "primary_category": hit.arxiv_eprints[0].categories[0],
-        "authors": hit.authors,
+        "doi": hit["dois"],
+        "recid": hit["control_number"],
+        "journal": hit["publication_info"][0]["journal_title"],
+        "creation_date": hit["record_creation_date"],
+        "primary_category": get_primary_category(hit),
+        "authors": hit["authors"],
     }
-
-
-def get_authors(dict):
-    return dict["authors"]
-
-
-def get_country_with_highest_gdp(countries, gdps):
-    values = [(country, gdps[country]) for country in countries]
-    return max(values, key=lambda t: t[1])[0]
-
-
-def get_most_important_country(countries, gdps):
-    hep_institutes = ["KEK", "SLAC", "DESY", "FERMILAB", "JINR"]
-    if len(countries) == 0:
-        return countries[0]
-    if "CERN" in countries:
-        return "CERN"
-    if len(present_hep := set(hep_institutes).intersection(set(countries))) > 0:
-        if len(present_hep) == 1:
-            return present_hep.pop()
-        return get_country_with_highest_gdp(
-            countries, gdps
-        )  # TODO: Change this to use HEP countries
-    return get_country_with_highest_gdp(countries, gdps)
-
-
-def get_gdp_from_file():
-    with open("gdp.csv", "r") as file:
-        reader = csv.reader(file)
-        ret_dict = {}
-        for row in reader:
-            value = row[3]
-            ret_dict[row[0]] = float(value) if value else 0
-        ret_dict["Taiwan"] = ret_dict["China"]
-        return ret_dict
-
-
-def build_countries_list_values(countries, all_countries_list):
-    ret_dict = {country: 0 for country in all_countries_list}
-    for country in countries:
-        ret_dict[country] += 1
-    ret_list = [value for value in ret_dict.items()]
-    ret_list.sort()
-    return [amount for _, amount in ret_list]
 
 
 parser = argparse.ArgumentParser()
@@ -93,232 +58,37 @@ if __name__ == "__main__":
     if password is None:
         raise Exception("Missing password argument.")
 
-    client = OpenSearch(
-        hosts=[{"host": host, "port": port, "url_prefix": "/es"}],
-        http_auth=(username, password),
-        use_ssl=False,
-        verify_certs=False,
-        ssl_assert_hostname=False,
-        ssl_show_warn=False,
-        connection_class=RequestsHttpConnection,
+    client = Elasticsearch(
+        hosts=[
+            {
+                "host": "es-scoap3-qa1.cern.ch",
+                "http_auth": (username, password),
+                "http_compress": True,
+                "port": 443,
+                "timeout": 60,
+                "url_prefix": "es",
+                "use_ssl": True,
+                "verify_certs": False,
+            }
+        ]
     )
-
-    s = (
-        Search(using=client, index="scoap3-records")
-        .extra(from_=0, size=10000)
-        .query(
-            "nested",
-            path="imprints",
-            query=Q(
-                "range",
-                **{
-                    "imprints.date": {
-                        "gt": f"{from_year}-01-01T00:00:00",
-                        "lt": f"{to_year}-01-01T00:00:00",
-                    }
-                },
-            ),
-        )
+    data = client.search(
+        index="scoap3-records",
+        scroll="2m",
+        _source=[
+            "dois",
+            "control_number",
+            "publication_info",
+            "record_creation_date",
+            "arxiv_eprints",
+            "authors",
+        ],
+        body={
+            "query": {"range": {"year": {"gte": f"{from_year}", "lte": f"{to_year}"}}}
+        },
     )
-    full_country_list = [
-        "Afghanistan",
-        "Albania",
-        "Algeria",
-        "Angola",
-        "Antigua and Barbuda",
-        "Argentina",
-        "Armenia",
-        "Australia",
-        "Austria",
-        "Azerbaijan",
-        "Bahamas",
-        "Bahrain",
-        "Bangladesh",
-        "Barbados",
-        "Belarus",
-        "Belgium",
-        "Belize",
-        "Benin",
-        "Bhutan",
-        "Bolivia",
-        "Bosnia and Herzegovina",
-        "Botswana",
-        "Brazil",
-        "Brunei",
-        "Bulgaria",
-        "Burkina Faso",
-        "Burundi",
-        "Cabo Verde",
-        "Cambodia",
-        "Cameroon",
-        "Canada",
-        "Caribbean small states",
-        "Central African Republic",
-        "CERN",
-        "Chad",
-        "Chile",
-        "China",
-        "Colombia",
-        "Comoros",
-        "Congo Dem. Rep.",
-        "Congo Rep.",
-        "Costa Rica",
-        "Cote d'Ivoire",
-        "Croatia",
-        "Cuba",
-        "Cyprus",
-        "Czech Republic",
-        "Denmark",
-        "DESY",
-        "Dominica",
-        "Dominican Republic",
-        "Ecuador",
-        "Egypt",
-        "El Salvador",
-        "Equatorial Guinea",
-        "Estonia",
-        "Ethiopia",
-        "FERMILAB",
-        "Fiji",
-        "Finland",
-        "France",
-        "Gabon",
-        "Gambia",
-        "Georgia",
-        "Germany",
-        "Ghana",
-        "Greece",
-        "Grenada",
-        "Guatemala",
-        "Guinea",
-        "Guinea-Bissau",
-        "Guyana",
-        "Haiti",
-        "Honduras",
-        "Hong Kong",
-        "HUMAN CHECK",
-        "Hungary",
-        "Iceland",
-        "India",
-        "Indonesia",
-        "Iran",
-        "Iraq",
-        "Ireland",
-        "Israel",
-        "Italy",
-        "Jamaica",
-        "Japan",
-        "JINR",
-        "Jordan",
-        "Kazakhstan",
-        "KEK",
-        "Kenya",
-        "Kiribati",
-        "Kosovo",
-        "Kuwait",
-        "Kyrgyz Republic",
-        "Latvia",
-        "Lebanon",
-        "Lesotho",
-        "Liberia",
-        "Libya",
-        "Lithuania",
-        "Luxembourg",
-        "Macao SAR China",
-        "Macedonia",
-        "Madagascar",
-        "Malawi",
-        "Malaysia",
-        "Maldives",
-        "Mali",
-        "Malta",
-        "Marshall Islands",
-        "Mauritania",
-        "Mauritius",
-        "Mexico",
-        "Micronesia Fed. Sts.",
-        "Moldova",
-        "Monaco",
-        "Mongolia",
-        "Montenegro",
-        "Morocco",
-        "Mozambique",
-        "Myanmar",
-        "Namibia",
-        "Nepal",
-        "Netherlands",
-        "New Zealand",
-        "Nicaragua",
-        "Niger",
-        "Nigeria",
-        "North Korea",
-        "Norway",
-        "Oman",
-        "Other small states",
-        "Pakistan",
-        "Palestine",
-        "Panama",
-        "Papua New Guinea",
-        "Paraguay",
-        "Peru",
-        "Philippines",
-        "Poland",
-        "Portugal",
-        "Puerto Rico",
-        "Qatar",
-        "San Marino",
-        "Romania",
-        "Russia",
-        "Rwanda",
-        "Samoa",
-        "Sao Tome and Principe",
-        "Saudi Arabia",
-        "Senegal",
-        "Serbia",
-        "Seychelles",
-        "Sierra Leone",
-        "Singapore",
-        "SLAC",
-        "Slovakia",
-        "Slovenia",
-        "Solomon Islands",
-        "South Africa",
-        "South Korea",
-        "Spain",
-        "Sri Lanka",
-        "St. Kitts and Nevis",
-        "St. Lucia",
-        "St. Vincent and the Grenadines",
-        "Sudan",
-        "Suriname",
-        "Swazilend",
-        "Sweden",
-        "Switzerland",
-        "Syria",
-        "Taiwan",
-        "Tajikistan",
-        "Tanzania",
-        "Thailand",
-        "Timor-Leste",
-        "Togo",
-        "Tonga",
-        "Trinidad and Tobago",
-        "Tunisia",
-        "Turkey",
-        "Turkmenistan",
-        "Uganda",
-        "UK",
-        "Ukraine",
-        "United Arab Emirates",
-        "Uruguay",
-        "USA",
-        "Uzbekistan",
-        "Venezuela",
-        "Vietnam",
-        "Yemen",
-        "Zambia",
-        "Zimbabwe",
-    ]
+    sid = data["_scroll_id"]
+    scroll_size = len(data["hits"]["hits"])
 
     with open("results.csv", "w") as file:
         writer = csv.writer(file)
@@ -332,33 +102,34 @@ if __name__ == "__main__":
         ] + full_country_list
         writer.writerow(header)
         gdps = get_gdp_from_file()
+        while scroll_size > 0:
+            hits = data["hits"]["hits"]
+            for hit in hits:
+                try:
+                    entry = map_hit(hit["_source"])
+                    authors_list = get_authors(entry)
+                    data_line = [
+                        entry["doi"][0]["value"],
+                        entry["recid"],
+                        entry["journal"],
+                        entry["creation_date"],
+                        entry["primary_category"],
+                        len(entry["authors"]),
+                    ]
+                    authors = entry["authors"]
+                    affiliations_per_author = get_affiliations(authors)
+                    countries = [
+                        get_most_important_country(affils, gdps)
+                        for affils in affiliations_per_author
+                    ]
+                    writer.writerow(
+                        data_line
+                        + build_countries_list_values(countries, full_country_list)
+                    )
+                except Exception as e:
+                    print("Exception occured", e)
 
-        for hit in s.scan():
-            entry = map_hit(hit)
-            authors_list = get_authors(entry)
-
-            data_line = [
-                entry["doi"][0]["value"],
-                entry["recid"],
-                entry["journal"],
-                entry["creation_date"],
-                "",
-                len(entry["authors"]),
-            ]
-            authors = entry["authors"]
-
-            affiliations_per_author = [
-                list(map(lambda x: x["country"], author["affiliations"]))
-                for author in authors
-            ]
-            countries = [
-                get_most_important_country(affils, gdps)
-                for affils in affiliations_per_author
-            ]
-
-            # TODO: build_countries_list_values should return percentages instead of absolute values.
-            writer.writerow(
-                data_line + build_countries_list_values(countries, full_country_list)
-            )
-
-            # TODO: Fix "missing scrollID" issue.
+            data = client.scroll(scroll_id=sid, scroll="2m")
+            sid = data["_scroll_id"]
+            scroll_size = len(data["hits"]["hits"])
+        client.clear_scroll(scroll_id=sid)
---
+++ country-share/utils.py
@@ -0,0 +1,86 @@
+import csv
+
+from inspire_utils.record import get_value
+
+
+def get_authors(dict):
+    return dict["authors"]
+
+
+def get_primary_category(hit):
+    return get_value(hit, "arxiv_eprints.categories[0][0]", default="no category")
+
+
+def get_affiliations(authors):
+    affiliations_per_author = [
+        (get_value(author, "affiliations", default=[{"country": "UNKNOWN"}]))
+        for author in authors
+    ]
+    countries_per_author = []
+    for affiliation in affiliations_per_author:
+        countries_per_author.append(
+            [
+                get_value(countries, "country", default="UNKNOWN") or ["UNKNOWN"]
+                for countries in affiliation
+            ]
+        )
+
+    for countries in countries_per_author:
+        if "Palestine" in countries:
+            index = countries.index("Palestine")
+            countries[index] = "West Bank and Gaza"
+        if "HUMAN CHECK" in countries:
+            index = countries.index("HUMAN CHECK")
+            countries[index] = "UNKNOWN"
+        if len(countries) == 0:
+            countries = ["UNKNOWN"]
+    return countries_per_author
+
+
+def get_country_with_highest_gdp(countries, gdps):
+    values = [(country, gdps[country]) for country in countries]
+    return max(values, key=lambda t: t[1])[0]
+
+
+def get_most_important_country(countries, gdps):
+    hep_institutes = {
+        "KEK": "Japan",
+        "SLAC": "USA",
+        "DESY": "Germany",
+        "FERMILAB": "USA",
+        "JINR": "Russia",
+    }
+    if len(countries) == 1:
+        return countries[0]
+    if "CERN" in countries:
+        return "CERN"
+    if len(present_hep := set(hep_institutes.keys()).intersection(set(countries))) > 0:
+        if len(present_hep) == 1:
+            return hep_institutes[present_hep.pop()]
+        countries_of_hep_insitutions = [
+            hep_institutes[hep_instition] for hep_instition in present_hep
+        ]
+        return get_country_with_highest_gdp(countries_of_hep_insitutions, gdps)
+    return get_country_with_highest_gdp(countries, gdps)
+
+
+def get_gdp_from_file():
+    with open("gdp.csv", "r") as file:
+        reader = csv.reader(file)
+        ret_dict = {}
+        for row in reader:
+            value = row[3]
+            ret_dict[row[0]] = float(value) if value else 0
+        ret_dict["Taiwan"] = ret_dict["China"]
+        return ret_dict
+
+
+def build_countries_list_values(countries, all_countries_list):
+    ret_dict = {country: 0 for country in all_countries_list}
+    for country in countries:
+        if country not in ret_dict:
+            ret_dict["UNKNOWN"] += 1
+        else:
+            ret_dict[country] += 1
+    ret_list = [value for value in ret_dict.items()]
+    return [(amount * 100 / len(countries)) for _, amount in ret_list]
