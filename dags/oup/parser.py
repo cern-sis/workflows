0fa12dc7c6d16eca9d148771ef7bb9e981f645ec
ErnestaP <ernesta.petraityte@yahoo.com>
2022-10-18 16:23:39 +0200

    parser (not full)

--- dags/oup/parser.py
+++ dags/oup/parser.py
@@ -1,20 +1,18 @@
 import xml.etree.ElementTree as ET
 
 from common.parsing.parser import IParser
-from common.parsing.xml_extractors import (
-    AttributeExtractor,
-    CustomExtractor,
-    TextExtractor,
-)
+from common.parsing.xml_extractors import AttributeExtractor, CustomExtractor
+from hindawi.xml_extractors import HindawiTextExtractor as TextExtractor
 from structlog import get_logger
 
 
 class OUPParser(IParser):
+    prefixes = {
+        "prefix": "http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd"
+    }
+
     def __init__(self) -> None:
         self.logger = get_logger().bind(class_name=type(self).__name__)
-        self.prefixes = {
-            "prefix": "http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd"
-        }
         self.article_type_mapping = {
             "research-article": "article",
             "corrected-article": "article",
@@ -30,17 +28,22 @@ class OUPParser(IParser):
                 source="prefix:front/prefix:article-meta/prefix:article-id/[@pub-id-type='doi']",
                 extra_function=lambda x: [x],
                 prefixes=self.prefixes,
+                default_value=[],
             ),
             CustomExtractor(
                 destination="journal_doctype",
                 extraction_function=self._get_journal_doctype,
+                default_value="",
             ),
             CustomExtractor(
                 destination="related_article_doi",
                 extraction_function=self._get_related_article_doi,
+                default_value=[],
             ),
             CustomExtractor(
-                destination="arxiv_eprints", extraction_function=self._get_arxiv_eprints
+                destination="arxiv_eprints",
+                extraction_function=self._get_arxiv_eprints,
+                default_value=[],
             ),
             AttributeExtractor(
                 destination="page_nr",
@@ -48,16 +51,31 @@ class OUPParser(IParser):
                 attribute="count",
                 extra_function=lambda x: int(x),
                 prefixes=self.prefixes,
+                default_value=[0],
             ),
-            CustomExtractor(
-                destination="abstract", extraction_function=self._get_abstract
+            TextExtractor(
+                destination="abstract",
+                source="prefix:front/prefix:article-meta/prefix:abstract/prefix:p",
+                extra_function=lambda x: x,
+                prefixes=self.prefixes,
+                default_value="",
             ),
-            # TextExtractor(
-            #     destination="abstract",
-            #     source="prefix:front/prefix:article-meta/prefix:abstract/prefix:p",
-            #     extra_function=lambda x: x,
-            #     prefixes=self.prefixes,
-            # )
+            TextExtractor(
+                destination="title",
+                source="prefix:front/prefix:article-meta/prefix:title-group/prefix:article-title",
+                extra_function=lambda x: x,
+                prefixes=self.prefixes,
+                default_value="",
+            ),
+            # Need an example with subtitle
+            TextExtractor(
+                destination="subtitle",
+                source="prefix:front/prefix:article-meta/prefix:title-group/prefix:subtitle",
+                extra_function=lambda x: x,
+                required=False,
+                prefixes=self.prefixes,
+                default_value="",
+            ),
         ]
 
         super().__init__(extractors)
--- tests/units/oup/test_parser.py
+++ tests/units/oup/test_parser.py
@@ -29,14 +29,12 @@ def test_dois(parsed_articles):
     dois = ["10.1093/ptep/ptac108", "10.1093/ptep/ptac120", "10.1093/ptep/ptac113"]
 
     for doi, article in zip(dois, parsed_articles):
-        assert "dois" in article
         assert article["dois"] == [doi]
 
 
 def test_journal_doctype(parsed_articles):
     journal_doctypes = ["article", "article", "article"]
     for journal_doctype, article in zip(journal_doctypes, parsed_articles):
-        assert "journal_doctype" in article
         assert article["journal_doctype"] == journal_doctype
 
 
@@ -47,23 +45,30 @@ def test_arxiv_eprints(parsed_articles):
         {"value": "2207.02498"},
     ]
     for arxiv_eprint, article in zip(arxiv_eprints, parsed_articles):
-        assert "arxiv_eprints" in article
         assert article["arxiv_eprints"] == arxiv_eprint
 
 
 def test_page_nr(parsed_articles):
     page_nrs = [27, 16, 13]
     for page_nr, article in zip(page_nrs, parsed_articles):
-        assert "page_nr" in article
         assert article["page_nr"] == page_nr
 
 
 def test_abstract(parsed_articles):
     abstracts = [
         "We construct open-closed superstring interactions based on the open-closed homotopy algebra structure. This provides a classical open superstring field theory on general closed-superstring-field backgrounds described by classical solutions of the nonlinear equation of motion of the closed superstring field theory. We also give the corresponding WZW-like action through the map connecting the homotopy-based and WZW-like formulations.",
-        "The",
-        "",
+        'The<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">KBc</ns0:italic>algebra is a subalgebra that has been used to construct classical solutions in Witten&#8217;s open string field theory, such as the tachyon vacuum solution. The main purpose of this paper is to give various operator sets that satisfy the<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">KBc</ns0:italic>algebra. In addition, since those sets can contain matter operators arbitrarily, we can reproduce the solution of Kiermaier, Okawa, and Soler, and that of Erler and Maccaferri. Starting with a single D-brane solution on the tachyon vacuum, we replace the original<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">KBc</ns0:italic>in it with an appropriate set to generate each of the above solutions. Thus, it is expected that the<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">KBc</ns0:italic>algebra, combined with the single D-brane solution, leads to a more unified description of classical solutions.',
+        'We study<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">F</ns0:italic>-wave bottom mesons in heavy quark effective theory. The available experimental and theoretical data is used to calculate the masses of<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">F</ns0:italic>-wave bottom mesons. The decay widths of bottom mesons are analyzed to find upper bounds for the associated couplings. We also construct Regge trajectories for our predicted data in the (<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">J, M</ns0:italic><ns0:sup xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">2</ns0:sup>) plane, and our results nicely fit on Regge lines. Our results may provide crucial information for future experimental studies.',
     ]
     for abstract, article in zip(abstracts, parsed_articles):
-        assert "abstract" in article
         assert article["abstract"] == abstract
+
+
+def test_title(parsed_articles):
+    page_nrs = [
+        "Open-closed homotopy algebra in superstring field theory",
+        'Generating string field theory solutions with matter operators from<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">KBc</ns0:italic>algebra',
+        'Study of<ns0:italic xmlns:ns0="http://specifications.silverchair.com/xsd/1/24/SCJATS-journalpublishing.xsd">F</ns0:italic>-wave bottom mesons in heavy quark effective theory',
+    ]
+    for page_nr, article in zip(page_nrs, parsed_articles):
+        assert article["title"] == page_nr
