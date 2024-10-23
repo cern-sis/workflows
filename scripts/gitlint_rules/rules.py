ade9d6da031eb4a77379facdda37a2de6db69994
Lorenzo Vagliano <lorenzo.ventura.vagliano@cern.ch>
2024-10-23 17:03:05 +0200

    fix: Utilized gitlint rules
    Signed-off-by: lorenzo

--- scripts/gitlint_rules/rules.py
+++ scripts/gitlint_rules/rules.py
@@ -2,39 +2,38 @@ import re
 
 from gitlint.rules import CommitRule, RuleViolation
 
-class SignedOffBy(CommitRule):
-    """This rule will enforce that each commit contains a "Signed-off-by" line."""
+# class SignedOffBy(CommitRule):
+#     """This rule will enforce that each commit contains a "Signed-off-by" line."""
 
-    name = "body-requires-signed-off-by"
-    id = "Workflows1"
+#     name = "body-requires-signed-off-by"
+#     id = "Workflows1"
 
-    def validate(self, commit):
-        for line in commit.message.body:
-            if line.startswith("Signed-off-by"):
-                return
+#     def validate(self, commit):
+#         for line in commit.message.body:
+#             if line.startswith("Signed-off-by"):
+#                 return
 
-        msg = "Body does not contain a 'Signed-Off-By' line"
-        return [RuleViolation(self.id, msg, line_nr=1)]
+#         msg = "Body does not contain a 'Signed-Off-By' line"
+#         return [RuleViolation(self.id, msg, line_nr=1)]
 
 
-class ApprovedSubject(CommitRule):
-    """Validate subject of each commit.
+# class ApprovedSubject(CommitRule):
+#     """Validate subject of each commit.
 
-    This rule will enforce that each commit starts with a "module: text" format.
-    The 'module' can be any alphanumeric word, and the message must start with a colon followed by a space.
-    """
+#     This rule will enforce that each commit starts with a "module: text" format.
+#     The 'module' can be any alphanumeric word, and the message must start with a colon followed by a space.
+#     """
 
-    name = "approved-subject-in-title"
-    id = "Workflows2"
+#     name = "approved-subject-in-title"
+#     id = "Workflows2"
 
-    MODULE_PATTERN = re.compile(r"^[a-zA-Z0-9_-]+: .+")
+#     MODULE_PATTERN = re.compile(r"^[a-zA-Z0-9_-]+: .+")
 
-    def validate(self, commit):
-        title = commit.message.title
+#     def validate(self, commit):
+#         title = commit.message.title
 
-        if not self.MODULE_PATTERN.match(title):
-            msg = "Subject does not follow 'module: text' format"
-            return [RuleViolation(self.id, msg, line_nr=1)]
+#         if not self.MODULE_PATTERN.match(title):
+#             msg = "Subject does not follow 'module: text' format"
+#             return [RuleViolation(self.id, msg, line_nr=1)]
 
-        return
-
+#         return
